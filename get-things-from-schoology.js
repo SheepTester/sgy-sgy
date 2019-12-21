c1fefa85ff19c99614ecb9221e12246b94d4c961
SheepTester <ussa.say@gmail.com>
2019-12-21 11:49:43 -0800

    Commenting and posting

--- get-things-from-schoology.js
+++ get-things-from-schoology.js
@@ -19,26 +19,45 @@ const oauth = OAuth({
   }
 })
 
-function fetch (url, method = 'GET') {
+function askSgy (url, body = null, method = body ? 'POST' : 'GET') {
   return new Promise((resolve, reject) => {
     request({
       url,
       method,
+      body: body && JSON.stringify(body),
       headers: {
         'Accept': 'application/json',
+        'Content-Type': 'application/json',
         ...oauth.toHeader(oauth.authorize({ url, method }))
       }
-    }, (err, response) => {
+    }, (err, { statusCode }, body) => {
       if (err) {
         reject(err)
-      } else if (response.statusCode !== 200) {
-        reject(new Error(`${colours.red(response.statusCode)}\n\n${response.body}`))
+      } else if (Math.floor(statusCode / 100) !== 2) { // outside of 200s range
+        reject(new Error(`${colours.red(statusCode)}\n\n${body}`))
       } else {
-        resolve(response)
+        resolve(body)
       }
     })
   })
 }
 
-fetch(base + '/schools')
-  .then(({ body }) => console.log(body))
+askSgy(base + '/group/553683519/updates', {
+  body: 'Epic<h1>EPIC</h1><trans>.lol',
+  "attachments": [
+    {
+      "type": "link",
+      "title": "<h1>epic</h1>",
+      "url": "https:\/\/www.example.com"
+    }
+  ]
+})
+  .then(async body => {
+    const { id } = JSON.parse(body)
+    for (let i = 0; i < 10; i++) {
+      await askSgy(base + '/group/553683519/updates/' + id + '/comments', {
+        comment: `ok ${i}er`,
+        uid: '2017219'
+      })
+    }
+  })
