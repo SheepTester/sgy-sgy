56d90cfe6e6659e49f58670c6c47febf6336a875
SheepTester <ussa.say@gmail.com>
2020-08-12 21:15:46 -0700

    zoom

---
+++ zoom/test.js
@@ -0,0 +1,44 @@
+const { clientId, clientSecret, redirect, code } = require('./credentials.json')
+const fetch = require('node-fetch')
+
+function toBase64 (str) {
+  return Buffer.from(str).toString('base64')
+}
+
+async function getToken () {
+  const params = new URLSearchParams()
+  params.set('grant_type', 'authorization_code')
+  params.set('code', code)
+  params.set('redirect_uri', redirect)
+  const json = await fetch('https://zoom.us/oauth/token?' + params, {
+    method: 'POST',
+    headers: {
+      Authorization: `Basic ${toBase64(clientId + ':' + clientSecret)}`
+    }
+  }).then(r => r.json())
+  console.log(json)
+  return {
+    accessToken: json.access_token,
+    refreshToken: json.refresh_token
+  }
+}
+
+async function main () {
+  const { accessToken } = await getToken()
+  console.log(await fetch('https://api.zoom.us/v2/users/me', {
+    headers: {
+      Authorization: `Bearer ${accessToken}`
+    }
+  }).then(r => r.json()))
+  {
+    const params = new URLSearchParams()
+    params.set('token', accessToken)
+    console.log(await fetch('https://zoom.us/oauth/revoke?' + params, {
+      method: 'POST',
+      headers: {
+        Authorization: `Basic ${toBase64(clientId + ':' + clientSecret)}`
+      }
+    }).then(r => r.json()))
+  }
+}
+main()
