8d3a1ad21ade3def0a5473492af3ec717cc2c965
SheepTester <ussa.say@gmail.com>
2020-09-02 23:03:50 -0700

    Racket has no OAuth-1.0a library, but I still want to translate the server to it

---
+++ oauth-3leg/servlet.rkt
@@ -0,0 +1,13 @@
+#lang racket
+
+(require web-server/servlet
+         web-server/servlet-env)
+
+(define (start req)
+  (response/xexpr
+   `(html (head (title "Hello world!"))
+          (body (p "Hey out there!")))))
+
+(serve/servlet start
+               #:launch-browser? #f
+               #:servlet-path "/")
