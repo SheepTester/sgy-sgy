9e03549402be03bbb0570b88fec56a50f4cdbd1a
SheepTester <ussa.say@gmail.com>
2020-09-10 20:49:09 -0700

    Schoology API is quite powerful (see more) I'll have to autolink "xyz fields" to their field definitions

---
+++ explorer/table-parser.js
@@ -0,0 +1,16 @@
+// Select the <table> element in inspect element
+(table => {
+const headings = Array.from(table.querySelectorAll('th'), d => d.textContent.trim().toLowerCase())
+let data = ''
+for (const row of [...table.querySelectorAll('tr')].slice(1)) {
+for (let i = 0; i < row.children.length; i++) {
+const val = row.children[i].textContent.trim()
+if (!val) continue
+data += i === 0 ? '- ' : '  '
+data += headings[i] + ': '
+data += val
+data += '\n'
+}
+}
+return data
+})($0)
