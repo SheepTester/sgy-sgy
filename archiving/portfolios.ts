13074fff73757754a7603d3d3f675f7098de44be
Sean <ussa.say@gmail.com>
2021-07-29 23:30:04 -0700

    archiving: Schoology time-sensitive download URLs annoy
    It's also inconsistent because it randomly gives 404 errors sometimes

--- archiving/portfolios.ts
+++ archiving/portfolios.ts
@@ -1,9 +1,11 @@
 // deno-lint-ignore-file camelcase
 import { ensureDir } from 'https://deno.land/std@0.97.0/fs/ensure_dir.ts'
+import { exists } from 'https://deno.land/std@0.103.0/fs/exists.ts'
 import { AnticipatedHttpError, cachePath, external } from './cache.ts'
 import * as html from './html-maker.ts'
+import { cookie, root } from './init.ts'
 import { me } from './me.ts'
-import { stringToPath } from './utilts.ts'
+import { delay, stringToPath } from './utilts.ts'
 
 type ApiPortfoliosInitResponse = {
   data: {
@@ -138,6 +140,39 @@ type ApiPortfolioResponse = {
   }
 }
 
+async function attemptUrlNoCache (
+  url: string,
+  csrfToken: string,
+): Promise<ApiPortfolioResponse> {
+  // Schoology is quite unreliable and inconsistent. It sometimes gives 404
+  // errors, but not always.
+  const response1: ApiPortfolioResponse | null = await fetch(url, {
+    headers: { 'X-Csrf-Token': csrfToken, cookie },
+  }).then(async r => {
+    if (r.ok) {
+      return r.json()
+    } else if (r.status === 429 || r.status === 500) {
+      console.log(
+        `${url} gave a HTTP ${r.status} error, so I will wait 5 seconds and try again.`,
+      )
+      return null
+    } else {
+      throw new Error(`HTTP ${r.status} error for ${r.url}: ${await r.text()}`)
+    }
+  })
+  if (response1) {
+    return response1
+  }
+  // Wait 5 seconds just in case
+  await delay(5000)
+  const response2: ApiPortfolioResponse = await fetch(url, {
+    headers: { 'X-Csrf-Token': csrfToken, cookie },
+  }).then(r =>
+    r.ok ? r.json() : Promise.reject(`HTTP ${r.status} error for ${r.url}`),
+  )
+  return response2
+}
+
 /** Get portfolios for a user ID */
 export async function archivePortfolios (
   userId: number,
@@ -163,21 +198,37 @@ export async function archivePortfolios (
       { headers: { 'X-Csrf-Token': csrfToken } },
     )
     await ensureDir(outPath)
-    for (const { file_info: fileInfo, metadata, id } of portfolio.items) {
-      if (fileInfo) {
-        await cachePath(external(fileInfo.tempURI), 'file', {
-          cachePath: `${outPath}${id}_${stringToPath(
+    let needDownload = false
+    outer: for (const item of portfolio.items) {
+      for (const fileInfo of [item.file_info, item.metadata.file_info]) {
+        if (fileInfo) {
+          const path = `${outPath}${item.id}_${stringToPath(
             fileInfo.filename.slice(0, fileInfo.filename.lastIndexOf('.')),
-          )}${fileInfo.filename.slice(fileInfo.filename.lastIndexOf('.'))}`,
-        })
+          )}${fileInfo.filename.slice(fileInfo.filename.lastIndexOf('.'))}`
+          if (!(await exists(path))) {
+            needDownload = true
+            break outer
+          }
+        }
       }
-      if (metadata.file_info) {
-        const fileInfo = metadata.file_info
-        await cachePath(external(fileInfo.tempURI), 'file', {
-          cachePath: `${outPath}${id}_${stringToPath(
-            fileInfo.filename.slice(0, fileInfo.filename.lastIndexOf('.')),
-          )}${fileInfo.filename.slice(fileInfo.filename.lastIndexOf('.'))}`,
-        })
+    }
+    if (needDownload) {
+      // Try to get unstale download links
+      const { data: portfolio } = await attemptUrlNoCache(
+        `${root}/portfolios/users/${userId}/portfolios/${id}`,
+        csrfToken,
+      )
+      for (const item of portfolio.items) {
+        for (const fileInfo of [item.file_info, item.metadata.file_info]) {
+          if (fileInfo) {
+            const path = `${outPath}${item.id}_${stringToPath(
+              fileInfo.filename.slice(0, fileInfo.filename.lastIndexOf('.')),
+            )}${fileInfo.filename.slice(fileInfo.filename.lastIndexOf('.'))}`
+            await cachePath(external(fileInfo.tempURI), 'file', {
+              cachePath: path,
+            })
+          }
+        }
       }
     }
     await Deno.writeTextFile(
