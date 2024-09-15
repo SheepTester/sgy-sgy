7405546610da403b5bd8c5b4c13160098a0e968a
Sean <ussa.say@gmail.com>
2024-09-14 19:09:16 -0700

    Get reports from a given day

--- explore/police/parse.ts
+++ explore/police/parse.ts
@@ -1,8 +1,6 @@
 // tsx explore/police/parse.ts
 
-import { readFile } from 'fs/promises'
 import { getDocument, VerbosityLevel } from 'pdfjs-dist'
-import { arrayEqual } from '../../lib/comp'
 
 type TextObject = {
   content: string
@@ -24,7 +22,7 @@ type Report = {
   disposition: string
 }
 
-const fields = [
+const FIELDS = [
   'Date Reported',
   'Incident/Case#',
   'Date Occurred',
@@ -33,89 +31,92 @@ const fields = [
   'Disposition'
 ]
 
-const reports: Report[] = []
+const BASE_URL =
+  'https://www.police.ucsd.edu/docs/reports/CallsandArrests/CallsForService/'
 
-const pdf = await getDocument({
-  // idk why pdf.js doesn't allow Buffer
-  data: new Uint8Array(
-    await readFile('/mnt/c/Users/seant/Downloads/OVERWRITE ME.pdf')
-  ),
-  useSystemFonts: true,
-  verbosity: VerbosityLevel.ERRORS
-}).promise
-for (let i = 1; i <= pdf.numPages; i++) {
-  const page = await pdf.getPage(i)
-  const { items } = await page.getTextContent()
-  const pageText = items
-    .filter(item => 'transform' in item)
-    .map(
-      (item): TextObject => ({
-        content: item.str,
-        hasEol: item.hasEOL,
-        x: item.transform[4],
-        y: item.transform[5]
-      })
-    )
-    .sort(
-      // Sort from top to bottom, then left to right
-      (a, b) => (Math.abs(a.y - b.y) > 0.1 ? b.y - a.y : a.x - b.x)
-    )
-    .map(t => t.content)
-    .filter(content => content.trim().length > 0)
+async function getReports (date: Date): Promise<Report[]> {
+  const reports: Report[] = []
+  const pdf = await getDocument({
+    url: `${BASE_URL}/${date.toLocaleString('en-US', {
+      dateStyle: 'long',
+      timeZone: 'UTC'
+    })}.pdf`,
+    useSystemFonts: true,
+    verbosity: VerbosityLevel.ERRORS
+  }).promise
+  for (let i = 1; i <= pdf.numPages; i++) {
+    const page = await pdf.getPage(i)
+    const { items } = await page.getTextContent()
+    const pageText = items
+      .filter(item => 'transform' in item)
+      .map(
+        (item): TextObject => ({
+          content: item.str,
+          hasEol: item.hasEOL,
+          x: item.transform[4],
+          y: item.transform[5]
+        })
+      )
+      .sort(
+        // Sort from top to bottom, then left to right
+        (a, b) => (Math.abs(a.y - b.y) > 0.1 ? b.y - a.y : a.x - b.x)
+      )
+      .map(t => t.content)
+      .filter(content => content.trim().length > 0)
 
-  if (
-    !arrayEqual(pageText.splice(0, 3), [
-      'UCSD POLICE DEPARTMENT',
-      'CRIME AND FIRE LOG/MEDIA BULLETIN',
-      'SEPTEMBER 9, 2024'
-    ])
-  ) {
-    throw new Error('page does not start with UCPD header')
-  }
-
-  const indices: number[] = []
-  for (const [i, content] of pageText.entries()) {
-    if (content === 'Date Reported') {
-      indices.push(i - 2)
+    if (
+      pageText[0] !== 'UCSD POLICE DEPARTMENT' &&
+      pageText[1] !== 'CRIME AND FIRE LOG/MEDIA BULLETIN'
+    ) {
+      throw new Error('page does not start with UCPD header')
     }
-  }
+    pageText.splice(0, 3)
 
-  for (let i = 0; i < indices.length; i++) {
-    const start = indices[i]
-    const end = indices[i + 1] ?? pageText.length
-    const parts: string[][] = []
-    let j = start
-    for (const field of fields) {
-      const fieldStart = j
-      for (; j < end; j++) {
-        // Sometimes they forget the colon after "Disposition"
-        if (pageText[j] === field || pageText[j] === field + ':') {
-          break
-        }
+    const indices: number[] = []
+    for (const [i, content] of pageText.entries()) {
+      if (content === 'Date Reported') {
+        indices.push(i - 2)
       }
-      parts.push(pageText.slice(fieldStart, j))
-      j++
     }
-    parts.push(pageText.slice(j, end))
-    const [
-      dateReported,
-      incidentCaseNum,
-      dateOccurred,
-      timeOccurred,
-      summary,
-      disposition
-    ] = parts.slice(1).map(segments => segments.join(' '))
-    reports.push({
-      type: pageText[start],
-      location: pageText[start + 1],
-      dateReported,
-      incidentCaseNum,
-      dateOccurred,
-      timeOccurred,
-      summary,
-      disposition
-    })
+
+    for (let i = 0; i < indices.length; i++) {
+      const start = indices[i]
+      const end = indices[i + 1] ?? pageText.length
+      const parts: string[][] = []
+      let j = start
+      for (const field of FIELDS) {
+        const fieldStart = j
+        for (; j < end; j++) {
+          // Sometimes they forget the colon after "Disposition"
+          if (pageText[j] === field || pageText[j] === field + ':') {
+            break
+          }
+        }
+        parts.push(pageText.slice(fieldStart, j))
+        j++
+      }
+      parts.push(pageText.slice(j, end))
+      const [
+        dateReported,
+        incidentCaseNum,
+        dateOccurred,
+        timeOccurred,
+        summary,
+        disposition
+      ] = parts.slice(1).map(segments => segments.join(' '))
+      reports.push({
+        type: pageText[start],
+        location: pageText[start + 1],
+        dateReported,
+        incidentCaseNum,
+        dateOccurred,
+        timeOccurred,
+        summary,
+        disposition
+      })
+    }
   }
+  return reports
 }
 
-console.log(reports)
+console.log(await getReports(new Date(Date.UTC(2024, 7, 13))))
