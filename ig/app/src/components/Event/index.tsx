7134bd84863a74a80427abc0e36ac0426aa0f667
Sean Yen (UCSD work laptop) <ussa.say@gmail.com>
2025-05-04 21:36:50 -0700

    live indicators

--- ig/app/src/components/Event/index.tsx
+++ ig/app/src/components/Event/index.tsx
@@ -18,7 +18,9 @@ export type EventObject = {
 }
 
 /** 1 hour */
-export const DEFAULT_EVENT_LENGTH = 1 * 60 * 60 * 10000
+export const DEFAULT_EVENT_LENGTH = 1 * 60 * 60 * 1000
+/** 1 day in ms */
+const DAY_LENGTH = 24 * 60 * 60 * 1000
 
 const fmt = new Intl.DateTimeFormat('en-US', {
   timeZone: 'UTC',
@@ -50,6 +52,16 @@ export function Event ({ event, now }: EventProps) {
       {event.url ? 'View source' : ''}
     </div>
   )
+  const parts = event.end
+    ? fmt.formatRangeToParts(event.start, event.end)
+    : fmt.formatToParts(event.start)
+  const index = parts.findIndex(part => part.type === 'year') + 1
+  const isToday =
+    now.today <= event.start.getTime() &&
+    event.start.getTime() < now.today + DAY_LENGTH
+  const end = event.end
+    ? event.end.getTime()
+    : event.start.getTime() + DEFAULT_EVENT_LENGTH
   return (
     <article className={styles.event}>
       <h3 className={styles.title}>
@@ -57,9 +69,20 @@ export function Event ({ event, now }: EventProps) {
       </h3>
       <span className={styles.timeIcon}>📅</span>
       <p className={styles.time}>
-        {event.end
-          ? fmt.formatRange(event.start, event.end)
-          : fmt.format(event.start)}
+        <span className={isToday ? styles.today : ''}>
+          {parts
+            .slice(0, index)
+            .map(part => part.value)
+            .join('')}
+        </span>
+        {parts
+          .slice(index)
+          .map(part => part.value)
+          .join('')}
+        &nbsp;
+        {event.start.getTime() <= now.now && now.now < end ? (
+          <span className={styles.liveIndicator} aria-label='(happening now)' />
+        ) : null}
       </p>
       <span className={styles.locationIcon}>📍</span>
       <p className={styles.location}>
