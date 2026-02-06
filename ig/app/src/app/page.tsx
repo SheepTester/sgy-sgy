2f33e706c55d21b9a6a86d77408914daf230d26b
sean (costco asus vivobook) <ussa.say@gmail.com>
2026-02-06 00:12:07 -0800

    make free food next.js site just a static website

--- ig/app/src/app/page.tsx
+++ ig/app/src/app/page.tsx
@@ -1,20 +1,7 @@
 import styles from './page.module.css'
-import { EventList } from '@/components/EventList'
 import Link from 'next/link'
-import { getEvents } from '@/util/getEvents'
 
-/** cache for 5 min */
-export const revalidate = 300
-
-const fmt = new Intl.DateTimeFormat('en-US', {
-  timeZone: 'America/Los_Angeles',
-  dateStyle: 'long',
-  timeStyle: 'long'
-})
-
-export default async function Home () {
-  const events = await getEvents()
-
+export default async function Home() {
   return (
     <>
       <h1 className={styles.heading}>
@@ -25,35 +12,12 @@ export default async function Home () {
         </span>
       </h1>
       <p className={styles.description}>
-        Every day, I scroll through posts and stories posted by UCSD orgs on
-        Instagram. I scan them with an LLM to get event information, but it's
-        wrong like more than half the time (a.k.a. correct some of the time :D),
-        so{' '}
-        <Link href='https://github.com/SheepTester/sgy-sgy/issues'>
-          prompt engineering ideas
-        </Link>{' '}
-        would be appreciated. If you don't trust AI (which is fair), I also have
-        an{' '}
-        <Link href='https://sheep.thingkingland.app/as-finance/'>
-          <strong>alternate list of free food</strong>
-        </Link>{' '}
-        that clubs are ordering using your tuition, pulled directly from
-        Associated Students. Let's get your tuition's worth!
-      </p>
-      <p className={styles.description}>
-        Message{' '}
-        <Link href='https://www.instagram.com/eventcollatorucsd/'>
-          @eventcollatorucsd
-        </Link>{' '}
-        if I'm missing any events. Check out the code on{' '}
-        <Link href='https://github.com/SheepTester/sgy-sgy/tree/master/ig'>
-          GitHub
+        I've moved to{' '}
+        <Link href='https://sheeptester.github.io/ucsd-free-food/'>
+          a new site
         </Link>
-        . Made by Chaitya and{' '}
-        <Link href='https://sheeptester.github.io/'>Sean</Link>. Page generated{' '}
-        {fmt.format(new Date())}. <Link href='/past'>See past events.</Link>
+        !
       </p>
-      <EventList events={events} mode='upcoming' />
     </>
   )
 }
--- ig/app/src/app/past/page.tsx
+++ ig/app/src/app/past/page.tsx
@@ -1,20 +1,7 @@
 import styles from '../page.module.css'
-import { EventList } from '@/components/EventList'
 import Link from 'next/link'
-import { getEvents } from '@/util/getEvents'
 
-/** cache for 5 min */
-export const revalidate = 300
-
-const fmt = new Intl.DateTimeFormat('en-US', {
-  timeZone: 'America/Los_Angeles',
-  dateStyle: 'long',
-  timeStyle: 'long'
-})
-
-export default async function Home () {
-  const events = await getEvents()
-
+export default async function Home() {
   return (
     <>
       <h1 className={styles.heading}>
@@ -25,11 +12,12 @@ export default async function Home () {
         </span>
       </h1>
       <p className={styles.description}>
-        Page generated {fmt.format(new Date())}. Because I'm hitting Vercel's
-        free plan limits, only the events from the past week are visible for
-        now. <Link href='/'>See upcoming events.</Link>
+        I've moved to{' '}
+        <Link href='https://sheeptester.github.io/ucsd-free-food/past/'>
+          a new site
+        </Link>
+        !
       </p>
-      <EventList events={events} mode='past' />
     </>
   )
 }
